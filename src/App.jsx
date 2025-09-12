04705168c10ed67db802fa6665307c2fc7ce71f8
BaoNG0105 <baongse184840@fpt.edu.vn>
2025-09-12 17:56:09 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        {/* <Hero />
+        <Content /> */}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App
