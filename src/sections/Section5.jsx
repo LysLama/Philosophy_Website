6ea3f94d3491ee9793241d43d6b0051097091f61
BaoNG0105 <baongse184840@fpt.edu.vn>
2025-09-14 23:33:32 +0700

    change content conclusion section

--- src/sections/Section5.jsx
+++ src/sections/Section5.jsx
@@ -8,8 +8,12 @@ const Section5 = () => {
             <div className="section-content">
                 <div className="content-text">
                     <p>
-                    Đấu tranh giai cấp là động lực trực tiếp trong các xã hội có đối kháng giai cấp. Nhưng khi tiến tới xã hội cộng sản chủ nghĩa – xã hội không còn đối kháng giai cấp – lịch sử nhân loại vẫn không dừng lại. Nó sẽ tiếp tục vận động bởi những động lực mới: phát triển lực lượng sản xuất, khoa học – công nghệ, dân chủ xã hội chủ nghĩa, hợp tác và nhu cầu nâng cao đời sống của con người (Giáo trình 2019, tr. 198).
+                        • Tóm lại, triết học Mác – Lênin khẳng định đấu tranh giai cấp là động lực trực tiếp, cơ bản của lịch sử trong các xã hội có sự phân chia giai cấp. Nó là cơ chế tất yếu để phá vỡ xiềng xích của quan hệ sản xuất cũ, mở đường cho tiến bộ xã hội. Tuy nhiên, vai trò động lực này chỉ mang tính lịch sử, gắn liền với "thời tiền sử" của nhân loại.
                     </p>
+                    <p>
+                        • Giả định về một xã hội không còn đối kháng giai cấp không có nghĩa là lịch sử sẽ kết thúc. Ngược lại, đó sẽ là một chương mới, một sự khởi đầu thực sự của lịch sử nhân văn. Động lực phát triển sẽ không mất đi mà chuyển hóa từ những mâu thuẫn đối kháng giữa người với người sang những mâu thuẫn không đối kháng giữa con người với tự nhiên, giữa cái tiến bộ và cái lạc hậu, giữa nhu cầu vô hạn và năng lực hữu hạn. Khi đó, sự phát triển không còn được thúc đẩy bởi sự thù hận và xung đột, mà bởi sự hợp tác, sáng tạo và khát vọng chinh phục những đỉnh cao mới của toàn thể cộng đồng. Đó là một viễn cảnh phát triển bền vững và mang tính người hơn bao giờ hết.
+                    </p>
+
                 </div>
             </div>
         </section>
